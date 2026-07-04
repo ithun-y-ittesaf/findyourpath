8d40eebfb231976c8d75aa5b1c10cc78158fa09b
ithun-y-ittesaf <ittesafyeasir@iut-dhaka.edu>
2026-07-04 10:50:22 +0600

    Added Dijkstra

---
+++ algorithms/search/dijkstra.py
@@ -0,0 +1,27 @@
+import heapq
+from algorithms.search.bfs import reconstruct_path
+
+def dijkstra (G, start, goal, get_neighbors):
+    # We all know what dijkstra does
+
+    frontier = [(0, start)]
+    came_from = {start: None}
+    cost_so_far = {start: 0}
+    nodes_expanded = 0
+
+    while frontier:
+        curr_dist, curr = heapq.heappop(frontier)
+        nodes_expanded += 1
+
+        if curr == goal:
+            return reconstruct_path(came_from, start, goal), nodes_expanded
+
+        for neighbor, weight in get_neighbors(G, curr):
+            new_dist = curr_dist + weight
+            if neighbor not in cost_so_far or new_dist < cost_so_far[neighbor]:
+                cost_so_far[neighbor] = new_dist
+                came_from[neighbor] = curr
+                heapq.heappush(frontier, (new_dist, neighbor))
+
+    return None, nodes_expanded
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.map_loader import load_map
 from src.graph_utils import get_neighbors
 from algorithms.search.bfs import bfs
+from algorithms.search.dijkstra import dijkstra
 
 G = load_map(bbox=(90.328217,23.700179,90.440483,23.826493))
 # print(G.number_of_nodes(), G.number_of_edges())
@@ -9,6 +10,22 @@ G = load_map(bbox=(90.328217,23.700179,90.440483,23.826493))
 nodes = list(G.nodes)
 start, goal = nodes[0], nodes[-1]
 
-path, expanded = bfs(G, start, goal, get_neighbors)
-print("Path: ", path)
+bfs_path, expanded = bfs(G, start, goal, get_neighbors)
+print("Path: ", bfs_path)
 print("Nodes expanded", expanded)
+
+print("----")
+
+dijkstra_path, expanded = dijkstra(G, start, goal, get_neighbors)
+print("Dijkstra path:", dijkstra_path)
+print("Dijkstra nodes expanded:", expanded)
+
+def path_length(G, path, get_neighbors):
+    total = 0
+    for i in range(len(path) - 1):
+        neighbors = dict(get_neighbors(G, path[i]))
+        total += neighbors[path[i+1]]
+    return total
+
+print("BFS path length (m):", path_length(G, bfs_path, get_neighbors))
+print("Dijkstra path length (m):", path_length(G, dijkstra_path, get_neighbors))
